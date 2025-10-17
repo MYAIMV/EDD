9d18d6a11ce6469103120e72dd1ba90b54b1bdc0
Mitzi Arrieta Ignacio <20243ds176@utez.edu.mx>
2025-10-17 08:58:15 -0600

    Add files via upload

---
+++ SINGLYLINKNEKLIST/src/SinglyLinkedList.java
@@ -0,0 +1,63 @@
+public class SinglyLinkedList {
+
+    Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public boolean contains(int data) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void remove(int data) {
+        if (head == null) {
+            return;
+        }
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while (current.next != null && current.next.data != data) {
+            current = current.next;
+        }
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
+    }
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " - ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public int countOccurrences(int data) {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
+                count++;
+            }
+            current = current.next;
+        }return count;
+    }
+}
