09eedf335c26dd73486bbabe5e52277ebf266818
Mitzi Arrieta Ignacio <20243ds176@utez.edu.mx>
2025-11-14 08:53:42 -0600

    Add files via upload
    Trabajo de clase

---
+++ Eliminar duplicados/App.java
@@ -0,0 +1,17 @@
+
+
+// =====MAIN======
+
+import java.util.Arrays;
+import trabajo.QuickSort;
+
+public class App {
+public static void main(String[] args) throws Exception {
+
+QuickSort sorting = new QuickSort();
+
+int[] arr={9, 7, 5, 11, 12, 2, 14, 3, 10, 6};
+sorting.quickSort(arr);
+System.out.println(Arrays.toString(arr));
+}
+}
